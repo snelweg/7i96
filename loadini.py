8de51305ac6fb4374950319982748b2281ab23a4
John Thornton <bjt128@gmail.com>
2018-07-31 11:38:56 -0500

    add more ini items
    Signed-off-by: John Thornton <bjt128@gmail.com>

--- loadini.py
+++ loadini.py
@@ -7,26 +7,36 @@ def iniList():
 	iniList.append(['JOINT_0', 'STEPSPACE', 'stepSpace_0'])
 	iniList.append(['JOINT_0', 'DIRSETUP', 'dirSetup_0'])
 	iniList.append(['JOINT_0', 'DIRHOLD', 'dirHold_0'])
+	iniList.append(['JOINT_0', 'MIN_LIMIT', 'jointMinLimit_0'])
+	iniList.append(['JOINT_0', 'MAX_LIMIT', 'jointMaxLimit_0'])
 
 	iniList.append(['JOINT_1', 'STEPLEN', 'stepTime_1'])
 	iniList.append(['JOINT_1', 'STEPSPACE', 'stepSpace_1'])
 	iniList.append(['JOINT_1', 'DIRSETUP', 'dirSetup_1'])
 	iniList.append(['JOINT_1', 'DIRHOLD', 'dirHold_1'])
+	iniList.append(['JOINT_1', 'MIN_LIMIT', 'jointMinLimit_1'])
+	iniList.append(['JOINT_1', 'MAX_LIMIT', 'jointMaxLimit_1'])
 
 	iniList.append(['JOINT_2', 'STEPLEN', 'stepTime_2'])
 	iniList.append(['JOINT_2', 'STEPSPACE', 'stepSpace_2'])
 	iniList.append(['JOINT_2', 'DIRSETUP', 'dirSetup_2'])
 	iniList.append(['JOINT_2', 'DIRHOLD', 'dirHold_2'])
+	iniList.append(['JOINT_2', 'MIN_LIMIT', 'jointMinLimit_2'])
+	iniList.append(['JOINT_2', 'MAX_LIMIT', 'jointMaxLimit_2'])
 
 	iniList.append(['JOINT_3', 'STEPLEN', 'stepTime_3'])
 	iniList.append(['JOINT_3', 'STEPSPACE', 'stepSpace_3'])
 	iniList.append(['JOINT_3', 'DIRSETUP', 'dirSetup_3'])
 	iniList.append(['JOINT_3', 'DIRHOLD', 'dirHold_3'])
+	iniList.append(['JOINT_3', 'MIN_LIMIT', 'jointMinLimit_3'])
+	iniList.append(['JOINT_3', 'MAX_LIMIT', 'jointMaxLimit_3'])
 
 	iniList.append(['JOINT_4', 'STEPLEN', 'stepTime_4'])
 	iniList.append(['JOINT_4', 'STEPSPACE', 'stepSpace_4'])
 	iniList.append(['JOINT_4', 'DIRSETUP', 'dirSetup_4'])
 	iniList.append(['JOINT_4', 'DIRHOLD', 'dirHold_4'])
+	iniList.append(['JOINT_4', 'MIN_LIMIT', 'jointMinLimit_4'])
+	iniList.append(['JOINT_4', 'MAX_LIMIT', 'jointMaxLimit_4'])
 
 	return iniList
 
