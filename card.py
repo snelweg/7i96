e14eea056288ae53c1f531dcc93bef60c6b9a4f4
John Thornton <bjt128@gmail.com>
2018-08-13 07:01:23 -0500

    test card
    Signed-off-by: John Thornton <bjt128@gmail.com>

--- card.py
+++ card.py
@@ -7,19 +7,21 @@ def check(parent):
 
 	if parent.ipAddressCB.currentText() == 'None':
 		parent.testConnectionLB.setText('An IP address must be selected')
-
+		return
+	ipAddress = parent.ipAddressCB.currentText()
 
 	if sys.maxsize > 2**32:
-		command = ["./mesaflash64", "--device", "7i96", "--addr", "10.10.10.10", "--readhmid"]
+		mesaflash = "./mesaflash64"
 	else:
-		command = ["./mesaflash32", "--device", "7i96", "--addr", "10.10.10.10", "--readhmid"]
+		mesaflash = "./mesaflash32"
+	command = [mesaflash, "--device", "7i96", "--addr", ipAddress, "--readhmid"]
 
 	try:
 		output = subprocess.check_output(command, stderr=subprocess.PIPE)
 		parent.testConnectionLB.setText(output)
 	except subprocess.CalledProcessError as e:
-		print('exit code: {}'.format(e.returncode))
-		print('stdout: {}'.format(e.output.decode(sys.getfilesystemencoding())))
+		#print('exit code: {}'.format(e.returncode))
+		#print('stdout: {}'.format(e.output.decode(sys.getfilesystemencoding())))
 		parent.testConnectionLB.setText(e.output.decode(sys.getfilesystemencoding()))
 
 
