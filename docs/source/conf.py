df376f7e8d5b8843685db2322a38404adcde24af
Kurt Jacobson <kcjengr@gmail.com>
2019-03-04 11:55:34 -0500

    add 'sphinx.ext.githubpages' extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ release = u'1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
 ]
