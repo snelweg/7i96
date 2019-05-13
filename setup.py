89cfa4bac639adc1a7324935ac9062fc514b2782
John Thornton <jthornton@gnipsel.com>
2019-05-13 05:29:13 -0500

    fix include

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import os
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+# list of (destination, source_file) tuples
+DATA_FILES = [('~/.local/bin', ['m7i76e/mesaflash64', 'm7i76e/mesaflash32',])]
+data_files = [(os.path.expanduser(dest), src_list) for dest, src_list in DATA_FILES]
 
 setup(
     name="7i96",
@@ -15,6 +20,7 @@ setup(
     download_url="https://github.com/jethornton/mct7i96/tarball/master",
     python_requires='>=3',
     packages=find_packages(),
+    data_files=data_files,
     include_package_data=True,
     entry_points={
         'gui_scripts': ['7i96=m7i96.7i96:main',],
